ca96e525f8669e9f3be8858bc1056a1e524630c9
Diego Haz <hazdiego@gmail.com>
2015-04-28 17:33:42 -0300

    Change to Jade view engine

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {

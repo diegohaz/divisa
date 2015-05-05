102afcef70158a0a1415f4428e6b6a4b60964cf4
Diego Haz <hazdiego@gmail.com>
2015-05-05 15:31:49 -0300

    Update code to serve only static content

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 8080;
 
-app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res) {
-  res.render('index');
-});
-
-app.listen(port, function() {
-  console.log('Our app is running on http://localhost:' + port);
-});
+app.listen(process.env.PORT || 8080);

ced6ae9f0ed75417fbbbe9506df91a14d7be6563
Diego Haz <hazdiego@gmail.com>
2015-05-09 18:17:34 -0300

    Add preload

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,37 @@
-var moments = require('/js/moments');
-var player = require('/js/Player');
-var game = require('/js/Game');
+var preload = new createjs.LoadQueue(false, '/img/');
+var manifest = [
+  {id: 'player', src: '/img/player.png'},
+  {id: 'map1', src: '/img/map1.png'},
+  {id: 'scene1bg', src: '/img/scene1bg.jpg'},
+  {id: 'table1', src: '/img/table1.png'},
+  {id: 'weapons1', src: '/img/weapons1.png'},
+  {id: 'window1', src: '/img/window1.png'},
+];
 
-game.stage = document.getElementById('game');
-game.moments = moments;
-game.player = player;
-game.controls = {
-  left: [37, 65, 97],
-  right: [39, 68, 100]
-};
+preload.loadManifest(manifest);
 
-game.start();
+preload.on('progress', function(evt) {
+  document.getElementById('loader').textContent = (evt.progress * 100).toFixed(1) + '%';
+});
 
-document.getElementById('restart').addEventListener('click', function() {
-  game.restart();
+preload.on('complete', function() {
+  document.getElementById('loader').remove();
+
+  var moments = require('/js/moments');
+  var player = require('/js/Player');
+  var game = require('/js/Game');
+
+  game.stage = document.getElementById('game');
+  game.moments = moments;
+  game.player = player;
+  game.controls = {
+    left: [37, 65, 97],
+    right: [39, 68, 100]
+  };
+
+  game.start();
+
+  document.getElementById('restart').addEventListener('click', function() {
+    game.restart();
+  });
 });
